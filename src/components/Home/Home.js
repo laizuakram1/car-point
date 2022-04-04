11b915a9a9b40c8c77a5f80aba88344f74a11ffb
laizuakram1 <laizuakram98@gmail.com>
2022-04-05 00:31:32 +0600

    hompage image, title and button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h2 className='text-2xl'>this is hompage...</h2>
+        <div className='Home'>
+           <div className='car-intro-container'>
+           <div className='car-title'>
+               <h2 className='text-5xl'><span className='text-orange-400'>Power</span> & Beauty</h2>
+               <h3 className='text-3xl'>Chose your Best Car Now</h3>
+               <br />
+               <button className='buy-btn'>Buy Now</button>
+           </div>
+           <div className='carImage'>
+              <img src={`https://img.freepik.com/free-vector/blue-sports-car-isolated-white-vector_53876-67418.jpg?w=740`} alt="" />
+           </div>
+           </div>
         </div>
     );
 };

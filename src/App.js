294781c2c796cde31bf46334a1a5f520714ec50f
laizuakram1 <laizuakram98@gmail.com>
2022-04-04 23:47:14 +0600

    add navbar without design

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import Blogs from './components/Blogs/Blogs';
 import About from './components/About/About';
 import './App.css';
 import NotFound from './components/NotFound/NotFound';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-          
+        <div className='navbar'>
+          <div>
+              <p className='text-3xl'>Laizu Car Point</p>
+          </div>
+          <div className='navLink'>
+              <Link to='/'>Home</Link>
+              <Link to='/review'>Review</Link>
+              <Link to='/dashboard'>Dashboard</Link>
+              <Link to='/blogs'>Blogs</Link>
+              <Link to='/about'>About</Link>
+          </div>
         </div>
     );
 };
